7ea2f02cef0cbf482367effeeb4eceb1d47dc8e9
Maaz <maazahmed@tummoc.com>
2023-12-27 11:33:48 +0530

    response was getting accessed before initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,17 @@
 import "./App.css";
 import Editor from "@monaco-editor/react";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Navbar from "./Components/Navbar";
 
 function App() {
-  const [userCode, setUserCode] = useState("");
+  const [userCode, setUserCode] = useState(``);
   const [userLang, setUserLang] = useState("");
+  const [userTheme, setUserTheme] = useState("vs-dark");
   const [fontSize, setFontSize] = useState(16);
   const [userInput, setUserInput] = useState("");
-  const [userOutput, setUserOutput] = useState("");
+  const [userOutput, setUserOutput] = useState(``);
   const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const handleBeforeUnload = (event) => {
-      event.preventDefault();
-      event.returnValue = "";
-      return "";
-    };
-
-    window.addEventListener("beforeunload", handleBeforeUnload);
-
-    return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-    };
-  }, []);
 
   const options = {
     fontSize: fontSize,
@@ -39,25 +26,35 @@ function App() {
   const handleInputChange = (e) => {
     setUserInput(e.target.value);
   };
+
   const compile = async () => {
     setIsLoading(true);
-    setUserOutput("");
-    if (userLang === "") {
+    console.log(userLang);
+    if (userLang.length == 0) {
       alert("Please select a language!");
-      setIsLoading(false);
     }
+    setUserOutput("");
     if (userCode === "") {
-      setIsLoading(false);
       return;
     }
-    const response = await axios.post("http://localhost:3001/compile", {
+    await axios.post("http://localhost:3001/compile", {
       code: userCode,
       lang: userLang,
       input: userInput,
-    });
-    setUserOutput(response.data.stderr || response.data.stdout);
-    setIsLoading(false);
+    })
+      .then((resp) => {
+        console.log(resp);
+        setUserOutput(resp.data.stderr || resp.data.stdout);
+      })
+      .catch((err) => {
+        console.log("Error in compiling", err)
+      })
+      .finally(() => {
+        setIsLoading(false);
+      })
   };
+  const lines = userOutput.split("\r\n");
+  console.log(lines);
   return (
     <div className="App flex w-[100%] h-[100vh] border">
       <div className="left-component relative w-[70%]">
@@ -65,7 +62,6 @@ function App() {
           compile={compile}
           userLang={userLang}
           setUserLang={setUserLang}
-          setUserCode={setUserCode}
         />
         <Editor
           height="100vh"
@@ -74,13 +70,6 @@ function App() {
           language={userLang}
           defaultLanguage="python"
           defaultValue="# Enter your code here"
-          value={
-            !userCode
-              ? userLang != "python"
-                ? "// write your code here"
-                : "# write your code here"
-              : userCode
-          }
           onChange={(value) => {
             setUserCode(value);
           }}
@@ -101,20 +90,16 @@ function App() {
         <h3>Output</h3>
         {isLoading ? (
           <div className="output border h-2/3 bg-white flex justify-center text-black p-2 font-medium overflow-auto">
-            <img src="/loader.svg" alt="...loading" />
+            <img src="/Ripple-0.7s-200px.svg" />
           </div>
         ) : (
-          <div className="output border h-2/3 bg-white text-black p-2 font-medium overflow-auto whitespace-pre">
-            {userOutput.includes("error") ? (
-              <div>Error: {userOutput.split("error")[1]}</div>
-            ) : (
-              userOutput?.split("\n").map((line, index) => (
-                <div key={index}>
-                  {line}
-                  <br />
-                </div>
-              ))
-            )}
+          <div className="output border h-2/3 bg-white text-black p-2 font-medium overflow-auto">
+            {lines.map((line, index) => (
+              <div key={index}>
+                {line}
+                <br />
+              </div>
+            ))}
           </div>
         )}
       </div>
